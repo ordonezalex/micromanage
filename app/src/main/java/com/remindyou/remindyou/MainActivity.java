37aaee072d9c59edf0bf3a8ab3c7b46d03523b81
Marisa Gomez <marisa.gomez25@gmail.com>
2015-02-21 16:22:18 -0500

    trying to merge

--- app/src/main/java/com/remindyou/remindyou/MainActivity.java
+++ app/src/main/java/com/remindyou/remindyou/MainActivity.java
@@ -1,11 +1,14 @@
 package com.remindyou.remindyou;
 
+import android.app.FragmentManager;
+import android.content.ClipData;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
