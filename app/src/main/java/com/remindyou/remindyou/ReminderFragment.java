dcd37badd70476cfb8b90f6187eb56a4f47ddab4
Nathan Hilliard <nhilliar@stetson.edu>
2015-02-22 08:58:39 -0500

    Work on fragment_reminder layout, also doing more cleanup in ReminderFragment

--- app/src/main/java/com/remindyou/remindyou/ReminderFragment.java
+++ app/src/main/java/com/remindyou/remindyou/ReminderFragment.java
@@ -16,14 +16,6 @@ import com.parse.ParseQuery;
 
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ReminderFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ReminderFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ReminderFragment extends Fragment {
 
     /** Constant for our arg id for when we pass in he reminderId into the fragment. */
@@ -31,8 +23,6 @@ public class ReminderFragment extends Fragment {
 
     /** The property where we will store the reminder id. */
     private String mReminderId;
-
-    private OnFragmentInteractionListener mListener;
 
     public ReminderFragment() {
         // Required empty public constructor
@@ -67,9 +57,9 @@ public class ReminderFragment extends Fragment {
         qry.findInBackground(new FindCallback<ParseObject>() {
             public void done(List<ParseObject> reminders, ParseException e) {
                 if (e == null) {
-                    // Do stuff
+                    reminders.get(0).getString("from");
                 } else {
-                    Log.wtf("ReminderFragment", "What a terrible failure!");
+                    Log.wtf("ReminderFragment", "No reminder with id " + mReminderId + " exists!");
                 }
             }
         });
@@ -80,46 +70,5 @@ public class ReminderFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_reminder, container, false);
-    }
-
-    public void onButtonPressed(Uri uri) {
-
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
     }
 }
